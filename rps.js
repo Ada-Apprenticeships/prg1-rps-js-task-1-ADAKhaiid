117d26d435ca18fbbad6f557afa30ca6da67e22d
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-16 14:03:45 +0000

    Draw Mechanism Complete

--- rps.js
+++ rps.js
@@ -1,17 +1,12 @@
 function rockPaperScissors(player1, player2) {
+  // Both a
+  if (player1 == player2) {
+    return "draw"
+  } 
 
-  
-   
 }
 
-
-
-
-
-
-
-
-
+console.log(rockPaperScissors("rock","rock"))
 
 // Leave this code here for the automated tests
 module.exports = {
