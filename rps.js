76fc972689f92b544e81e0cf763eecb1546a0344
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-16 14:26:12 +0000

    Finalised Into Dictionary

--- rps.js
+++ rps.js
@@ -1,36 +1,29 @@
 function rockPaperScissors(player1, player2) {
-  // Both players draw
-  if (player1 == player2) {
-    return "draw"
+  // Ways Players can win using Dictionary 
+  const wins = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["spock", "paper"],
+    spock: ["scissors", "rock"],
+  };
+
+  // Players Draw
+  if (player1 === player2) {
+    return "draw";
   } 
-  // Player 1 Wins Scenarios
   
-  // Player 1 uses paper
-  if (player1 == "paper" && (player2 == "rock" || player2 == "spock")) {
-    return "player1"
-  }
-  // Player 1 uses rock
-  else if (player1 == "rock" && (player2 == "scissors" || player2 == "lizard"))  { 
-    return "player1"
-  }
-  // Player 1 uses scissors
-  else if (player1 == "scissors" && (player2 == "paper" || player2 == "lizard")) {
-    return "player1"
-  }
-  // Player 1 uses lizard
-  else if (player1 == "lizard" && (player2 == "spock" || player2 == "paper" )) {
-    return "player1"
-  }
-  // Player 1 uses spock
-  else if (player1 == "spock" && (player2 == "scissors" ||player2 == "rock")) {
-    return "player1"
-  }
+  // Ways Player 1 wins using the dictionary and keys
+  else if (wins[player1].includes(player2)) {
+    return "player1";
+  } 
   // If the above statements are not found true:
-  else 
-    return "player2"
+  else {
+    return "player2";
+  }
 }
 
-console.log(rockPaperScissors("lizard","rock"))
+console.log(rockPaperScissors("lizard", "rock"));
 
 // Leave this code here for the automated tests
 module.exports = {
