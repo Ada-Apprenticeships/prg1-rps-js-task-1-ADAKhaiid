f4d2baafe43fbba1b497cdc78994508b058e7689
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-16 14:13:03 +0000

    BIg Bang Theory - Shortened

--- rps.js
+++ rps.js
@@ -3,58 +3,28 @@ function rockPaperScissors(player1, player2) {
   if (player1 == player2) {
     return "draw"
   } 
-  
   // Player 1 Wins Scenarios
-
-  if (player1 == "paper" && player2 == "rock") {
-    return "player1"
-  }
-
-  else if (player1 == "rock" && player2 == "scissors" )  { 
-    return "player1"
-  }
   
-  else if (player1 == "scissors" && player2 == "paper") {
+  // Player 1 uses paper
+  if (player1 == "paper" && (player2 == "rock" || player2 == "spock")) {
     return "player1"
   }
-
-  else if (player1 == "paper" && player2 == "spock") {
+  // Player 1 uses rock
+  else if (player1 == "rock" && (player2 == "scissors" || player2 == "lizard"))  { 
+    return "player1"
+  }
+  // Player 1 uses scissors
+  else if (player1 == "scissors" && (player2 == "paper" || player2 == "lizard")) {
+    return "player1"
+  }
+  // Player 1 uses lizard
+  else if (player1 == "lizard" && (player2 == "spock" || player2 == "paper" )) {
+    return "player1"
+  }
+  // Player 1 uses spock
+  else if (player1 == "spock" && (player2 == "scissors" ||player2 == "rock")) {
       return "player1"
-    }
-
-  else if (player1 == "rock" && player2 == "lizard") {
-      return "player1"
-    }
-
-  else if (player1 == "scissors" && player2 == "lizard") {
-      return "player1"
-    }
-
-  else if (player1 == "spock" && player2 == "scissors") {
-      return "player1"
-    }
-
-  else if (player1 == "spock" && player2 == "rock") {
-      return "player1"
-    }
-
-  else if (player1 == "lizard" && player2 == "spock") {
-      return "player1"
-    }
-
-  else if (player1 == "lizard" && player2 == "paper") {
-      return "player1"
-    }
-
-  else if (player1 == "paper" && player2 == "spock") {
-      return "player1"
-    }
-
-  else if (player1 == "rock" && player2 == "lizard") {
-      return "player1"
-    }
-
-
+  }
   // If the above statements are not found true:
   return "player2"
 }
