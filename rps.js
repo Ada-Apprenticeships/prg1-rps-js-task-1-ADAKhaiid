43320a0d775b4c603539550432c5cb6d70c7475b
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-16 14:08:38 +0000

    RPS Complete

--- rps.js
+++ rps.js
@@ -1,12 +1,23 @@
 function rockPaperScissors(player1, player2) {
-  // Both a
+  // Both players draw
   if (player1 == player2) {
     return "draw"
   } 
+  
+  if (player1 == "paper" && player2 == "rock") {
+    return "player1"
+  }
 
+  else if (player1 == "rock" && player2 == "scissors" )  { 
+    return "player1"
+  }
+  
+  else if (player1 == "scissors" && player2 == "paper") {
+    return "player1"
+  }
 }
 
-console.log(rockPaperScissors("rock","rock"))
+console.log(rockPaperScissors("scissors","rock"))
 
 // Leave this code here for the automated tests
 module.exports = {
