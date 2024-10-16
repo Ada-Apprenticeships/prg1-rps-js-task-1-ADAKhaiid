530eaf793ec1ed72ccdf1ab5e86e21e58b877d24
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-16 14:12:08 +0000

    Big Bang Theory Complete

--- rps.js
+++ rps.js
@@ -1,10 +1,12 @@
 function rockPaperScissors(player1, player2) {
-  // Both players draw
+  // Both a
   if (player1 == player2) {
     return "draw"
   } 
   
-  else if (player1 == "paper" && player2 == "rock") {
+  // Player 1 Wins Scenarios
+
+  if (player1 == "paper" && player2 == "rock") {
     return "player1"
   }
 
@@ -16,12 +18,48 @@ function rockPaperScissors(player1, player2) {
     return "player1"
   }
 
-  else {
-    return "player2"
-  }
+  else if (player1 == "paper" && player2 == "spock") {
+      return "player1"
+    }
+
+  else if (player1 == "rock" && player2 == "lizard") {
+      return "player1"
+    }
+
+  else if (player1 == "scissors" && player2 == "lizard") {
+      return "player1"
+    }
+
+  else if (player1 == "spock" && player2 == "scissors") {
+      return "player1"
+    }
+
+  else if (player1 == "spock" && player2 == "rock") {
+      return "player1"
+    }
+
+  else if (player1 == "lizard" && player2 == "spock") {
+      return "player1"
+    }
+
+  else if (player1 == "lizard" && player2 == "paper") {
+      return "player1"
+    }
+
+  else if (player1 == "paper" && player2 == "spock") {
+      return "player1"
+    }
+
+  else if (player1 == "rock" && player2 == "lizard") {
+      return "player1"
+    }
+
+
+  // If the above statements are not found true:
+  return "player2"
 }
 
-console.log(rockPaperScissors("paper","rock"))
+console.log(rockPaperScissors("lizard","rock"))
 
 // Leave this code here for the automated tests
 module.exports = {
